1b5312a42d70e7aacbf05ff8174a558a7a8be7c3
brainerg <brainerg83@gmail.com>
2026-03-19 23:55:04 -0500

    1.

--- taller_3/src/views/HomeView.jsx
+++ taller_3/src/views/HomeView.jsx
@@ -1,7 +1,15 @@
 import Hero from '../components/Hero'
+import Courses from '../components/Courses'
+import Counter from '../components/Counter'
 
 function HomeView() {
-  return <Hero />
+  return (
+    <>
+      <Hero />
+      <Courses />
+      <Counter />
+    </>
+  )
 }
 
 export default HomeView
